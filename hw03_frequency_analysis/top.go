2edc7c566d8bd1cbc1ccdcdbb70221dabf9c6d0e
BorisPlus <boris-plus@main.ru>
2023-04-21 03:58:55 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,86 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+var LIMIT = 10
+
+// Частота встречаемости слова
+// в виде структуры.
+type WordObject struct {
+	word  string // Слово
+	count uint   // Частота встречаемости
+}
+
+// Частота встречаемости слов
+// в виде map.
+type DistributionMap struct {
+	mappedStatValue map[string]uint
+}
+
+// Метод получения Мap из строки.
+func (distMap *DistributionMap) Calculate(values []string) {
+	for _, word := range values {
+		if word == "" {
+			continue
+		}
+		distMap.mappedStatValue[word]++
+	}
+}
+
+// Метод представления Мap в виде []WordObject.
+func (distMap *DistributionMap) AsWordObjectStructs() []WordObject {
+	ws := []WordObject{}
+	for word, count := range distMap.mappedStatValue {
+		if word == "" {
+			continue
+		}
+		ws = append(ws, WordObject{word, count})
+	}
+	return ws
+}
+
+func Top10(s string) []string {
+	// Все слова без иных пробельных символов
+	s = strings.ReplaceAll(s, "\n", " ")
+	s = strings.ReplaceAll(s, "\t", " ")
+	s = strings.ReplaceAll(s, "\r", " ")
+	s = strings.ReplaceAll(s, "\f", " ")
+	s = strings.ReplaceAll(s, "\v", " ")
+	stringWords := strings.Split(s, " ")
+	// fmt.Println("stringWords", stringWords)
+
+	// Распределение в формате MAP key-value: слово -> его частота
+	distMap := DistributionMap{make(map[string]uint)}
+	distMap.Calculate(stringWords)
+	// fmt.Println("distribution", distMap)
+	wordsObjects := distMap.AsWordObjectStructs()
+	// fmt.Println("wordsObjects", wordsObjects)
+
+	// Сортировка в условии задачи: `ORDER BY count DESC, lexical ASC`
+	sort.Slice(
+		wordsObjects,
+		func(i, j int) bool {
+			return wordsObjects[i].count > wordsObjects[j].count || (wordsObjects[i].count == wordsObjects[j].count &&
+				wordsObjects[i].word < wordsObjects[j].word)
+		})
+	// Синтаксический сахар
+	orderedWordsObjects := wordsObjects
+	// fmt.Println("wordsObjects", wordsObjects)
+
+	wordsLen := len(orderedWordsObjects)
+	iter := 0
+	returnedWords := []string{}
+	// Для отладки: returnedWordsObjects := []WordObject{}
+	for iter < LIMIT && iter < wordsLen {
+		returnedWords = append(returnedWords, wordsObjects[iter].word)
+		// Для отладки: returnedWordsObjects = append(returnedWordsObjects, wordsObjects[iter])
+		iter++
+	}
+
+	// fmt.Println("returnedWords", returnedWords)
+	// fmt.Println("returnedWordsObjects", returnedWordsObjects)
+	return returnedWords
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -48,6 +48,19 @@ func TestTop10(t *testing.T) {
 		require.Len(t, Top10(""), 0)
 	})
 
+	t.Run("positive test at HW example", func(t *testing.T) {
+		expected := []string{
+			"and",     // 2
+			"one",     // 2
+			"cat",     // 1
+			"cats",    // 1
+			"dog,",    // 1
+			"dog,two", // 1
+			"man",     // 1
+		}
+		require.Equal(t, expected, Top10("cat and dog, one dog,two cats and one man"))
+	})
+
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
@@ -77,6 +90,38 @@ func TestTop10(t *testing.T) {
 				"то",        // 4
 			}
 			require.Equal(t, expected, Top10(text))
+		}
+	})
+
+	t.Run("negative test", func(t *testing.T) {
+		if !taskWithAsteriskIsCompleted {
+			expected := []string{
+				"а",         // 8
+				"он",        // 8
+				"и",         // 6
+				"ты",        // 5
+				"что",       // 5
+				"в",         // 4
+				"его",       // 4
+				"если",      // 4
+				"кристофер", // 4
+				"не",        // 4
+			}
+			require.NotEqual(t, expected, Top10(text))
+		} else {
+			expected := []string{
+				"он",        // 8
+				"а",         // 6
+				"и",         // 6
+				"ты",        // 5
+				"что",       // 5
+				"-",         // 4
+				"Кристофер", // 4
+				"если",      // 4
+				"не",        // 4
+				"то",        // 4
+			}
+			require.NotEqual(t, expected, Top10(text))
 		}
 	})
 }
