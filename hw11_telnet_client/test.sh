698e3f32b97bae73b7a0f073ed6fecc4ddb4d558
BorisPlus <boris-plus@main.ru>
2023-07-12 00:24:29 +0300

    * wtf

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -3,12 +3,12 @@ set -xeuo pipefail
 
 go build -o go-telnet.goc
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 > /tmp/nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 > ./nc.out &
 NC_PID=$!
 
 sleep 1
 
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet.goc localhost 4242 > /tmp/telnet.out &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet.goc localhost 4242 > ./telnet.out &
 TL_PID=$!
 
 sleep 5
@@ -24,14 +24,14 @@ function fileEquals() {
 expected_nc_out='I
 am
 TELNET client'
-fileEquals /tmp/nc.out "${expected_nc_out}"
+fileEquals ./nc.out "${expected_nc_out}"
 
 expected_telnet_out='...Try connect to localhost:4242
 ...Connected to localhost:4242
 Hello
 From
 NC'
-fileEquals /tmp/telnet.out "${expected_telnet_out}"
+fileEquals ./telnet.out "${expected_telnet_out}"
 
 rm -f go-telnet.goc
 echo "PASS"
