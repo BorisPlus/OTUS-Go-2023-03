b64c29e4fbf2e3855757c1c335327bff30eabf13
BorisPlus <boris-plus@main.ru>
2023-07-14 01:45:25 +0300

    * as chunks

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,7 +1,6 @@
 package hw10programoptimization
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"regexp"
@@ -16,20 +15,22 @@ func RowParser(
 	compiledRegexp *regexp.Regexp,
 	wg *sync.WaitGroup,
 	mtx *sync.Mutex,
-	syncMap *sync.Map,
+	domainStat *DomainStat,
+	// syncMap *sync.Map,
 ) {
 	defer wg.Done()
 	for rowData := range row {
 		matches := compiledRegexp.FindAllSubmatch(rowData, -1)
 		for matcheIndex := range matches {
 			domainAtLowercase := strings.ToLower(string(matches[matcheIndex][1]))
-			// mtx.Lock()
-			// mtx.Unlock()
-			_ = mtx
-			v, exsist := syncMap.LoadOrStore(domainAtLowercase, 1)
-			if exsist {
-				syncMap.Store(domainAtLowercase, v.(int)+1)
-			}
+			mtx.Lock()
+			(*domainStat)[domainAtLowercase]++
+			mtx.Unlock()
+			// _ = mtx
+			// v, exsist := syncMap.LoadOrStore(domainAtLowercase, 1)
+			// if exsist {
+			// 	syncMap.Store(domainAtLowercase, v.(int)+1)
+			// }
 		}
 	}
 }
@@ -44,31 +45,46 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	mtx := sync.Mutex{}
 	rowsChannel := make(chan []byte)
 
-	var syncMap sync.Map
+	// var syncMap sync.Map
+
+	domainStat := make(DomainStat)
 
 	for i := 0; i < workersCount; i++ {
 		wg.Add(1)
-		go RowParser(rowsChannel, compiledRegexp, &wg, &mtx, &syncMap)
+		go RowParser(rowsChannel, compiledRegexp, &wg, &mtx, &domainStat)
 	}
 
-	scanner := bufio.NewScanner(r)
-	const maxCapacity int = 10000000 // It's over 64K !!!
-	buf := make([]byte, maxCapacity)
-	scanner.Buffer(buf, maxCapacity)
-
-	for scanner.Scan() {
-		rowsChannel <- scanner.Bytes()
+	content, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
 	}
+	contentLenFeature := len(content) - 1
+	chunk := make([]byte, 100)
+	for i, b := range content {
+		if b == '\n' || contentLenFeature == i {
+			rowsChannel <- chunk
+			chunk = make([]byte, 100)
+			continue
+		} 
+		chunk = append(chunk, b)
+	}
+
+	// scanner := bufio.NewScanner(r)
+	// const maxCapacity int = 10000000 // It's over 64K !!!
+	// buf := make([]byte, maxCapacity)
+	// scanner.Buffer(buf, maxCapacity)
+
+	// for scanner.Scan() {
+	// 	rowsChannel <- scanner.Bytes()
+	// }
 
 	close(rowsChannel)
 	wg.Wait()
 
-	syncMapDomainStat := make(DomainStat)
+	// syncMap.Range(func(key, value interface{}) bool {
+	// 	syncMapDomainStat[key.(string)] = value.(int)
+	// 	return true
+	// })
 
-	syncMap.Range(func(key, value interface{}) bool {
-		syncMapDomainStat[key.(string)] = value.(int)
-		return true
-	})
-
-	return syncMapDomainStat, nil
+	return domainStat, nil
 }
