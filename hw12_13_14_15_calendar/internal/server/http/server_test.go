2bdde852b9658ed0d4c0fb71f41867c9e2a72125
BorisPlus <boris-plus@main.ru>
2023-07-25 22:55:36 +0300

    * http wotk in GitHub?

--- hw12_13_14_15_calendar/internal/server/http/server_test.go
+++ hw12_13_14_15_calendar/internal/server/http/server_test.go
@@ -33,8 +33,7 @@ func SendWithContext(ctx context.Context, body io.Reader) error {
 	return nil
 }
 
-func NotForGitHubTestServerCode(t *testing.T) {
-	t.Helper()
+func TestServerCode(t *testing.T) {
 	host := "localhost"
 	var port uint16 = 8081
 	outputInto := &bytes.Buffer{}
