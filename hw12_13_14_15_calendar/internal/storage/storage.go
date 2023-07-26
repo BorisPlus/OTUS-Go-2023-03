2e16aba1fd048d6bda21e860546f3123e3661896
BorisPlus <boris-plus@main.ru>
2023-07-26 22:19:48 +0300

    * lint

--- hw12_13_14_15_calendar/internal/storage/storage.go
+++ hw12_13_14_15_calendar/internal/storage/storage.go
@@ -6,7 +6,7 @@ import (
 	pgsqldtb "hw12_13_14_15_calendar/internal/storage/pgsqldtb"
 )
 
-func NewStorageByType(storageType string, a... any) interfaces.Storager {
+func NewStorageByType(storageType string, a ...any) interfaces.Storager {
 	switch storageType {
 	case "gomemory":
 		return gomemory.NewStorage()
