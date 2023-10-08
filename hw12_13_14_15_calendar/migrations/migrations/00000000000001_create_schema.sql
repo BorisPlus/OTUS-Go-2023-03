8e7d284fb5c4baf3be4443c23f89a107d6aea140
b <b@localhost>
2023-10-09 00:29:55 +0300

    * hw15_calendar

--- hw12_13_14_15_calendar/migrations/migrations/00000000000001_create_schema.sql
+++ hw12_13_14_15_calendar/migrations/migrations/00000000000001_create_schema.sql
@@ -1,7 +1,8 @@
 -- +goose Up
 CREATE SCHEMA IF NOT EXISTS hw15calendar;
+ALTER SCHEMA hw15calendar OWNER TO hw15user;
 -- SET search_path = hw15calendar;
 -- +goose Down
 -- SET search_path = public;
-DROP SCHEMA hw15calendar CASCADE;
+DROP SCHEMA IF EXISTS hw15calendar CASCADE;
 
